f8f887bdf912f15a6d631e22b93366a739b588da
hieu <hieungo1898@gmail.com>
2022-09-16 16:02:33 +0700

    update

---
+++ src/data/base_dataset.py
@@ -0,0 +1,37 @@
+# ====================================================
+# Dataset
+# ====================================================
+def prepare_input(cfg, text):
+    inputs = cfg.tokenizer.encode_plus(
+        text, 
+        return_tensors=None, 
+        add_special_tokens=True, 
+        max_length=CFG.max_len,
+        pad_to_max_length=True,
+        truncation=True
+    )
+    for k, v in inputs.items():
+        inputs[k] = torch.tensor(v, dtype=torch.long)
+    return inputs
+
+
+class TrainDataset(Dataset):
+    def __init__(self, cfg, df):
+        self.cfg = cfg
+        self.texts = df['full_text'].values
+        self.labels = df[cfg.target_cols].values
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, item):
+        inputs = prepare_input(self.cfg, self.texts[item])
+        label = torch.tensor(self.labels[item], dtype=torch.float)
+        return inputs, label
+    
+
+def collate(inputs):
+    mask_len = int(inputs["attention_mask"].sum(axis=1).max())
+    for k, v in inputs.items():
+        inputs[k] = inputs[k][:,:mask_len]
+    return inputs
---
+++ src/data/cv_split.py
@@ -0,0 +1,8 @@
+# ====================================================
+# CV split
+# ====================================================
+Fold = MultilabelStratifiedKFold(n_splits=CFG.n_fold, shuffle=True, random_state=CFG.seed)
+for n, (train_index, val_index) in enumerate(Fold.split(train, train[CFG.target_cols])):
+    train.loc[val_index, 'fold'] = int(n)
+train['fold'] = train['fold'].astype(int)
+display(train.groupby('fold').size())
